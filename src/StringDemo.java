01bffa620c1001b93aef004328e7eee6c3d15ad5
Radhika8390 <radhika.dhayalan83@gmail.com>
2023-06-09 03:20:21 -0400

    summerholidays

--- src/StringDemo.java
+++ src/StringDemo.java
@@ -2,5 +2,8 @@ public class StringDemo {
     public static void main(String[] args) {
         int x=10;
         Integer integerVar=new Integer(10);
+        Integer integerVar=new Integer(10);
+        Integer integerVar=new Integer(10);
+        Integer integerVar=new Integer(10);
     }
 }
